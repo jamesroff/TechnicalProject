aa6fb370c579cca27a49745506b32c86b6bb1dc7
James Roff <jr15832@my.bristol.ac.uk>
2019-03-07 13:08:44 +0000

    Added calculation of final collision time and geometric series graphs

--- QuarterWaveSimulation.m
+++ QuarterWaveSimulation.m
@@ -1,4 +1,4 @@
-function [t,y] = QuarterWaveSimulation(p,init,x_range,t_range,model)
+function [t,y,t_col,t_end] = QuarterWaveSimulation(p,init,x_range,t_range,model)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -9,18 +9,18 @@ t = t_range(1); y = init;
 
 opts = odeset('Events',@(t,y) Collision(t,y,x_range) );
 
+t_col = [];
+
 while t(end) ~= t_range(2)
     
     init = y(end,:);
     init(2) = - r * init(2);
     
     if (init(1) - x_range(1) <= 1e-5) && (init(2) <= 1e-3)
+        break   
+    elseif (abs (init(1) - x_range(2)) <= 1e-5) && (init(2) >= -1e-3)
         break
     end
-
-%     if init(1) - x_range < 0
-%         break
-%     end
     
     switch model
         case 'Initial'
@@ -29,10 +29,18 @@ while t(end) ~= t_range(2)
             [t1,y1] = ode45(@(t,y) FullDerivative(t,y,p(1:end-1)), [t(end),t_range(2)], init, opts);
     end
     
+    t_col = [t_col, t1(end)];
+        
     %fprintf('Restarting\n')
     
     t = [t; NaN; t1]; y = [y; NaN([1,size(y,2)]); y1];
 end
+
+if t_col(end) == t_range(2)
+    t_col = t_col(1:end-1);
+end
+
+t_end = CalculateEndTime(t_col);
 
 y = y';
 
@@ -40,30 +48,20 @@ end
 
 %% Functions
 
-% Derivative function
-% function [dydt] = Derivative(~,y,a,b)
-%     dydt = zeros(size(y));
-%     size(y)
-%     
-%     dydt(1,:) = y(2,:);
-%     dydt(2,:) = - y(2,:) + a.*y(3,:).*( y(1,:).^2 - 1 );
-%     dydt(3,:) = b*(1 - y(1,:).*sqrt(y(3,:)));
-% end
-
-% Event function
-% function [v,t,d] = Collision(~,y,x_range)
-%     v = [y(1) - x_range(1),...
-%          y(1) - x_range(2),...
-%          x_range(1) < y(1) && y(1) < x_range(2)];
-%     %y(1)
-%     %x_range(1) < y(1) && y(1) < x_range(2)
-%     t = [1,1,1];
-%     d = [-1,+1,0];
-% end
-
 function [v,t,d] = Collision(~,y,x_range)
     v = [real( y(1) ) - x_range(1),...
          real( y(1) ) - x_range(2)];
     t = [1,1];
     d = [-1,+1];
+end
+
+function [t_end] = CalculateEndTime(t)
+    if size(t,2) < 3
+        t_end = Inf;
+        return
+    end
+    r = (t(end)-t(end-1))/(t(end-1)-t(end-2));
+    a = t(end-1) - t(end-2);
+    timeToEnd = a/(1-r);
+    t_end = t(end-2) + timeToEnd;
 end
