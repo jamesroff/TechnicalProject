dd6bb5aaa7d3fda2bec2861290fd935853f5e5a5
James Roff <jr15832@my.bristol.ac.uk>
2019-04-05 15:39:11 +0100

    Close to Hopf bifurcation boundary

--- QWMSimulationScript.m
+++ QWMSimulationScript.m
@@ -13,7 +13,7 @@ q = 0.6;
 %%% Very weird behaviour with L = 9 - L = 10 - L = 11 for 'spring'
 L = 20; % 0.3, 0.35, 0.5
 x_range = [0,1.1];
-t_range = [0,100];
+t_range = [0,700];
 %t_range = t_range(1):(5e-3):t_range(2);
 %t_range = linspace(t_range(1),t_range(2),4000);
 
@@ -59,6 +59,9 @@ plot(t,y(1,:),'b-')
 %plot(t2,y2(1,:))
 xlabel('Time'); ylabel('Valve position')
 legend('Initial','Quarter Wave Model','location','northeast')
+%pbaspect([3 1 1])
+%set(gcf,'Position',[216 93 560 420])
+%set(gcf,'Position',[216 93 670 260])
 
 figure;
 plot(t_control,y_control(2,:),'r--')
@@ -92,6 +95,49 @@ plot(t,y(5,:),'b-')
 xlabel('Time'); ylabel('Velocity fluctuation')
 legend('Initial','Quarter Wave Model','location','northwest')
 
+% % %%
+% % figure;
+% % set(gcf,'Position',[216 93 560 680])
+% % 
+% % fig1 = subplot(4,1,1);
+% % plot(t_control,y_control(1,:),'r--')
+% % hold on
+% % plot(t,y(1,:),'b-')
+% % %plot(t2,y2(1,:))
+% % y1 = ylabel('Valve position');
+% % fig1.XTickLabel = [];
+% % 
+% % fig2 = subplot(4,1,2);
+% % plot(t_control,y_control(2,:),'r--')
+% % hold on
+% % plot(t,y(2,:),'b-')
+% % %plot(t2,y2(2,:))
+% % y2 = ylabel('Valve velocity');
+% % fig2.XTickLabel = [];
+% % 
+% % fig3 = subplot(4,1,3);
+% % plot(t_control,y_control(4,:),'r--')
+% % hold on
+% % plot(t,y(4,:),'b-')
+% % %plot(t2,y2(4,:))
+% % y3 = ylabel('Pressure fluctuation');
+% % fig3.XTickLabel = [];
+% % 
+% % subplot(4,1,4)
+% % plot(t_control,y_control(5,:),'r--')
+% % hold on
+% % plot(t,y(5,:),'b-')
+% % %plot(t2,y2(5,:))
+% % xlabel('Time'); y4 = ylabel('Velocity fluctuation');
+% % %legend('Initial','Quarter Wave Model','location','northwest')
+% % 
+% % % 
+% % yc1 = y1.Position; yc2 = y2.Position; yc3 = y3.Position; yc4 = y4.Position;
+% % xpos = min([yc1(1), yc2(1), yc3(1), yc4(1)]);
+% % yc1(1) = xpos; set(y1,'Pos',yc1)
+% % yc2(1) = xpos; set(y2,'Pos',yc2)
+% % yc3(1) = xpos; set(y3,'Pos',yc3)
+% % yc4(1) = xpos; set(y4,'Pos',yc4)
 
 % %% FFT
 % 
